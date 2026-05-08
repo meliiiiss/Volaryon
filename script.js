5ce2ee119f8ab43035f526b25c908cf67498abb7
meliiiiss <melissabatgut@gmail.com>
2026-05-07 21:48:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-const frases = [
-  "O mundo não acabou… ele apenas foi reescrito.",
-  "Após o despertar dos Núcleos Primordiais, a realidade se fragmentou.",
-  "Criaturas surgiram das sombras entre dimensões esquecidas.",
-  "Deuses antigos observam em silêncio… esperando.",
-  "Apenas os Despertos podem atravessar o Véu."
-];
+window.onload = function () {
 
-let i = 0;
-const story = document.getElementById("story");
-const btn = document.getElementById("enterBtn");
+  const frases = [
+    "O mundo não acabou… ele apenas foi reescrito.",
+    "Após o despertar dos Núcleos Primordiais, a realidade se fragmentou.",
+    "Criaturas surgiram das sombras entre dimensões esquecidas.",
+    "Deuses antigos observam em silêncio… esperando.",
+    "Apenas os Despertos podem atravessar o Véu."
+  ];
 
-function mostrarFrase() {
-  if (i < frases.length) {
-    story.innerHTML = frases[i];
-    i++;
-    setTimeout(mostrarFrase, 3000);
-  } else {
-    btn.classList.remove("hidden");
+  let i = 0;
+
+  const story = document.getElementById("story");
+  const btn = document.getElementById("enterBtn");
+
+  function mostrarFrase() {
+    if (i < frases.length) {
+      story.innerHTML = frases[i];
+      i++;
+      setTimeout(mostrarFrase, 3000);
+    } else {
+      btn.classList.remove("hidden");
+    }
   }
-}
 
-window.onload = () => {
   mostrarFrase();
 };

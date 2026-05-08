29c3beb4bbb13feaeee02e3927e4af1e67c82b84
meliiiiss <melissabatgut@gmail.com>
2026-05-07 21:56:43 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+
+  const frases = [
+    "O mundo não acabou… ele apenas foi reescrito.",
+    "Após o despertar dos Núcleos Primordiais, a realidade se fragmentou.",
+    "Criaturas surgiram das sombras entre dimensões esquecidas.",
+    "Deuses antigos observam em silêncio… esperando.",
+    "Apenas os Despertos podem atravessar o Véu."
+  ];
+
+  let i = 0;
+
+  const story = document.getElementById("story");
+  const btn = document.getElementById("enterBtn");
+
+  function mostrarFrase() {
+    if (i < frases.length) {
+      story.innerHTML = frases[i];
+      i++;
+      setTimeout(mostrarFrase, 3000);
+    } else {
+      btn.classList.remove("hidden");
+    }
+  }
+
+  mostrarFrase();
+};
+console.log("JS carregou");
+console.log(document.getElementById("story"));
